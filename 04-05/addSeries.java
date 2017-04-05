af5f15c23862696e64b86d6723e8a6ac1d596286
Samuel Stickels <stickels@cs.grinnell.edu>
2017-04-05 13:43:21 -0500

    finished reading exercise on adding a series to a tree

---
+++ 04-05/addSeries.java
@@ -0,0 +1,26 @@
+//a.
+   Trees<Integer> tree = new Trees<>();
+   tree.add(5);
+   tree.add(8);
+   tree.add(9);
+   tree.add(6);
+   tree.add(2);
+   tree.add(4);
+   tree.add(1);
+
+//b.
+   Trees<Integer> tree = new Trees<>();
+   tree.add(4);
+   tree.add(2);
+   tree.add(9);
+   tree.add(15);
+   tree.add(20);
+   tree.add(6);
+   tree.add(5);
+//c.
+   Trees<Integer> tree = new Trees<>();
+   tree.add(9);
+   tree.add(6);
+   tree.add(5);
+   tree.add(2);
+   tree.add(1);
